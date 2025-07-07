f3345ccab536cb425140769a4c2e9d46866573f4
Avinash <35265478+SRE-Avinash@users.noreply.github.com>
2025-07-07 05:45:33 +0000

    initial vers

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import * as core from '@actions/core';
 const name = core.getInput('name');
 const output_value = `Hello ${name}`;
 core.setOutput('greeting', 'output_value');
+
+
+try {
+  // Do stuff
+}
+catch (err) {
+  // setFailed logs the message and sets a failing exit code
+  core.setFailed(`Action failed with error ${err}`);
+}
